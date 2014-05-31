108dca9ad3a1d25b20e5aa81d81618fee205b580
Joseph Zhou <fangtee@qq.com>
2014-05-31 22:30:45 +0800

    deploy

---
+++ PtPHP/Tests/Lib/PtRedisTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joseph
+ * Date: 14-5-15
+ * Time: 下午1:24
+ */
+
+namespace Lib;
+
+
+class PtRedisTest extends \PHPUnit_Framework_TestCase {
+    var $cache;
+    function __construct(){
+        $config = array(
+            "host"=>"127.0.0.1",
+            "port"=>6379,
+        );
+        $this->cache = new PtCacheRedis($config);
+    }
+    function setUp(){
+
+    }
+    function test_set(){
+        $res = $this->cache->set("test","safsdsfs");
+        $this->assertEquals($res,true);
+    }
+
+    function test_get(){
+        $value =  $this->cache->get("test");
+        PtConsole::log($value);
+    }
+    function test_del(){
+        $this->cache->del("test");
+        echo  $this->cache->get("test");
+    }
+
+    function test_page(){
+        for ($i=0;$i<25000;$i++)
+        	$this->cache->lpush('mylist',$i);
+
+        $length = $this->cache->obj->llen('mylist');
+
+        $pagesize = 5;
+        $pageno = max(1,7);
+        $totalpage = ceil($length/$pagesize);
+        $start = ($pageno-1)*$pagesize;
+        $end = $start+$pagesize;
+
+        $lists = $this->cache->obj->lrange('mylist',$start,$end);
+
+        var_dump($lists);
+    }
+}
+ 
